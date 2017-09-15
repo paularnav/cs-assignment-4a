f081812371854c695508fe9ffc4d9228bea4acdc
paularnav <31165622+paularnav@users.noreply.github.com>
2017-09-15 22:27:51 +0530

    Create a4q1.cpp

---
+++ a4q1.cpp
@@ -0,0 +1,16 @@
+# cs-assignment-4a
+    #include <iostream>
+    using namespace std;
+     
+    void cube(int a)
+    {int b;
+    b=a*a*a;
+    cout<<"\n The cube of the number is "<<b;}
+     
+    int main() {
+    	int n;
+    	cout<<"Enter the number ";
+    	cin>>n;
+    	cube(n);
+    	return 0;
+    }
